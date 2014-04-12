159965ad128f50041624e872c102ce5275d77080
Michael Arlt <qwertologe@gmail.com>
2014-04-12 20:52:10 +0200

    Bugfix path to cutz

--- test/test_cutz.rb
+++ test/test_cutz.rb
@@ -3,7 +3,7 @@
 $cmd='../bin/cutz'
 
 def test_cutz(params,assertExitstatus,assertText=nil)
-  resultText=`../cutz #{params} test_cutz.txt 2>&1`
+  resultText=`#{$cmd} #{params} test_cutz.txt 2>&1`
   resultExitstatus = $?.exitstatus
   failed=false
   if (assertExitstatus and resultExitstatus != 0) or (!assertExitstatus and resultExitstatus == 0)
@@ -18,16 +18,19 @@ def test_cutz(params,assertExitstatus,assertText=nil)
 end
 
 test_cutz '-abcde',false
-test_cutz '-f-1,1',true,"789 123
+test_cutz '-f-1,1',true,'789 123
 c a
 
 1234567890 1234567890
-"
+'
 
-test_cutz '-f-1..-2 -o:',true,"789:456
+test_cutz '-f-1..-2 -o:',true,'789:456
 c:b
 
 
-"
+'
 
-
+#test_cutz '-c-1..1',true,'987  654 321
+#c    b   a
+#
+#0987654321'
